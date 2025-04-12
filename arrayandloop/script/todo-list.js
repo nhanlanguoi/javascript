402b23a37f132996e6956cc1a840fbc035d940db
nhanpro <nhunguyenthanhnhu83@gmail.com>
2025-04-12 08:45:54 +0700

    finish arrayandloop

--- arrayandloop/script/todo-list.js
+++ arrayandloop/script/todo-list.js
@@ -12,7 +12,7 @@ function taotieude1() {
 let tolist = "";
 for(var i = 0 ;i< k.length;i++){
     const t = k[i];
-    const html = `<p>${t} <button onclick ="k.splice(${i},1) ;taotieude1(); ">delete</button></p>`;
+    const html = `<p>${t.name}<span style = "padding-left:50px">${t.name1}</span> <button onclick ="k.splice(${i},1) ;taotieude1(); ">delete</button></p>`;
     tolist += html;
 }
 console.log(tolist);
@@ -21,10 +21,17 @@ document.querySelector('.dv1').innerHTML = tolist;
 
 function addto(){
     const name = document.querySelector('.p').value;
-    k.push(name);
+    const name1 = document.querySelector('.p1').value;
+    k.push({
+        // name:name,
+        // dat:name1,
+        name,
+        name1,
+    });
     document.querySelector('.p').value = '';
     taotieude1();
 }
+
 
         
 
