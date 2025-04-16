a60328999a04dbfec275699a9d07188c5d4b172a
nhanpro <nhunguyenthanhnhu83@gmail.com>
2025-04-16 15:59:17 +0700

    use products.js file data

--- javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project-main/script/amazon.js
@@ -1,34 +1,43 @@
 console.log('hello');
 
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name:  "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars:4.5,
-            count:87,
-        },
-        priceCent:1090
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name:  "Intermediate Size Basketball",
-        rating: {
-            stars:4,
-            count:127,
-        },
-        priceCent:2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name:  "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            stars:4.5,
-            count:56,
-        },
-        priceCent:799
-    }
-];
+// const products = [
+//     {
+//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//         name:  "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//         rating: {
+//             stars:4.5,
+//             count:87,
+//         },
+//         priceCent:1090
+//     },
+//     {
+//         image: "images/products/intermediate-composite-basketball.jpg",
+//         name:  "Intermediate Size Basketball",
+//         rating: {
+//             stars:4,
+//             count:127,
+//         },
+//         priceCent:2095
+//     },
+//     {
+//         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//         name:  "Adults Plain Cotton T-Shirt - 2 Pack",
+//         rating: {
+//             stars:4.5,
+//             count:56,
+//         },
+//         priceCent:799
+//     },
+//     {
+//         image: "images/products/black-2-slot-toaster.jpg",
+//         name:  "2 Slot Toaster - Black",
+//         rating: {
+//             stars:5,
+//             count:2197,
+//         },
+//         priceCent:1899
+//     }
+// ];
 
 let productHtml =``
 products.forEach((product)=>{
@@ -55,7 +64,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${(product.priceCent/100).toFixed(2)} <!--get two number behind dot-->
+            $${(product.priceCents/100).toFixed(2)} <!--get two number behind dot-->
           </div>
 
           <div class="product-quantity-container">
